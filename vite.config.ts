1357e432e7178f9959c6a529628d7d979e47bf80
Sebastian Luna <sebastianbarraganluna@gmail.com>
2025-06-13 02:58:12 -0500

    Add tailwindcss to project

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslintPlugin from '@nabla/vite-plugin-eslint'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), eslintPlugin()]
+  plugins: [vue(), tailwindcss(), eslintPlugin()]
 })
