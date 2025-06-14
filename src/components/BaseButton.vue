3c58fa63d8b5077261a56fd10ff9d9ab4c56c08e
Sebastian Luna <sebastianbarraganluna@gmail.com>
2025-06-14 02:59:45 -0500

    Adjust padding button

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -3,7 +3,7 @@ defineProps<{ content: string }>()
 </script>
 
 <template>
-  <button class="py-3 px-7 bg-poke-red font-bold text-lg text-white rounded-full cursor-pointer hover:bg-poke-red-dark">
+  <button class="py-3 px-4 bg-poke-red font-bold text-lg text-white rounded-full cursor-pointer hover:bg-poke-red-dark">
     {{ content }}
   </button>
 </template>
