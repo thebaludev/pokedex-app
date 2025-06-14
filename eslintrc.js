8f9c79c175549ebf8d780eb2f3d42de4eb928b7c
Sebastian Luna <sebastianbarraganluna@gmail.com>
2025-06-13 21:31:34 -0500

    Install eslint pkg to to improve vue lint Adjust eslint and ts config

--- eslintrc.js
+++ eslintrc.js
@@ -1,18 +1,23 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    browser: true,
+    node: true,
+    es2021: true
   },
   parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2021,
+    sourceType: 'module'
+  },
   extends: [
     'standard',
     'plugin:vue/vue3-recommended',
     'plugin:@typescript-eslint/recommended'
   ],
-  parserOptions: {
-    parser: '@typescript-eslint/parser',
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: ['vue', '@typescript-eslint']
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {
+    'vue/html-quotes': ['error', 'double']
+  }
 }
